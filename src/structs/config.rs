7990fac1c704ad16097c0c8bd7d249e052e9d3ef
Carsten Østergaard <carsteno@uia.no>
2024-05-14 17:00:34 +0200

    added clone function for config struct

--- src/structs/config.rs
+++ src/structs/config.rs
@@ -13,3 +13,22 @@ pub(crate) struct Config {
     pub(crate) config: Vec<ConfigSet>,
 }
 
+
+impl Clone for ConfigSet {
+    fn clone(&self) -> Self {
+        ConfigSet {
+            bearer_token: self.bearer_token.clone(),
+            group_id: self.group_id.clone(),
+            user_id: self.user_id.clone(),
+            discord_webhook: self.discord_webhook.clone(),
+        }
+    }
+}
+
+impl Clone for Config {
+    fn clone(&self) -> Self {
+        Config {
+            config: self.config.clone()
+        }
+    }
+}
